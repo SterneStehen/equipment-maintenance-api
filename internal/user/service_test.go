70a8aeb6af2645d10f6546847500ec3dc03947ba
SterneStehen <122315689+SterneStehen@users.noreply.github.com>
2023-03-09 16:43:05 +0300

    add login check for user

---
+++ internal/user/service_test.go
@@ -0,0 +1,129 @@
+package user
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type fakeStore struct {
+	createFn  func(context.Context, string, string, string) (User, error)
+	byEmailFn func(context.Context, string) (User, error)
+	byIDFn    func(context.Context, int64) (User, error)
+}
+
+func (f fakeStore) Create(ctx context.Context, email, hash, name string) (User, error) {
+	return f.createFn(ctx, email, hash, name)
+}
+
+func (f fakeStore) ByEmail(ctx context.Context, email string) (User, error) {
+	return f.byEmailFn(ctx, email)
+}
+
+func (f fakeStore) ByID(ctx context.Context, id int64) (User, error) {
+	return f.byIDFn(ctx, id)
+}
+
+type fakePasswords struct {
+	hashFn    func(string) (string, error)
+	matchesFn func(string, string) bool
+}
+
+func (f fakePasswords) Hash(password string) (string, error) {
+	return f.hashFn(password)
+}
+
+func (f fakePasswords) Matches(hash, password string) bool {
+	return f.matchesFn(hash, password)
+}
+
+func TestRegisterNormalizesInputAndHashesPassword(t *testing.T) {
+	var gotEmail, gotHash, gotName string
+	repo := fakeStore{
+		createFn: func(_ context.Context, email, hash, name string) (User, error) {
+			gotEmail, gotHash, gotName = email, hash, name
+			return User{ID: 1, Email: email, PasswordHash: hash, FullName: name, Role: RoleAdmin}, nil
+		},
+	}
+	svc := NewService(repo)
+	svc.pass = fakePasswords{
+		hashFn: func(password string) (string, error) {
+			assert.Equal(t, "strong-password", password)
+			return "hashed-value", nil
+		},
+	}
+
+	u, err := svc.Register(context.Background(), RegisterInput{
+		Email: "  Person@Example.COM ", Password: "strong-password", FullName: "  Pat Smith  ",
+	})
+
+	require.NoError(t, err)
+	assert.Equal(t, "person@example.com", gotEmail)
+	assert.Equal(t, "hashed-value", gotHash)
+	assert.NotEqual(t, "strong-password", gotHash)
+	assert.Equal(t, "Pat Smith", gotName)
+	assert.Equal(t, RoleAdmin, u.Role)
+}
+
+func TestRegisterValidation(t *testing.T) {
+	tests := []struct {
+		name string
+		in   RegisterInput
+		want error
+	}{
+		{name: "bad email", in: RegisterInput{Email: "nope", Password: "password1", FullName: "Pat"}, want: ErrInvalidEmail},
+		{name: "short password", in: RegisterInput{Email: "p@example.com", Password: "short", FullName: "Pat"}, want: ErrInvalidPassword},
+		{name: "bcrypt limit", in: RegisterInput{Email: "p@example.com", Password: string(make([]byte, 73)), FullName: "Pat"}, want: ErrInvalidPassword},
+		{name: "blank name", in: RegisterInput{Email: "p@example.com", Password: "password1", FullName: "   "}, want: ErrInvalidName},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			svc := NewService(fakeStore{})
+			_, err := svc.Register(context.Background(), tt.in)
+			require.ErrorIs(t, err, tt.want)
+		})
+	}
+}
+
+func TestAuthenticate(t *testing.T) {
+	stored := User{ID: 8, Email: "person@example.com", PasswordHash: "stored-hash", Role: RoleViewer}
+	repo := fakeStore{
+		byEmailFn: func(_ context.Context, email string) (User, error) {
+			require.Equal(t, "person@example.com", email)
+			return stored, nil
+		},
+	}
+	svc := NewService(repo)
+	svc.pass = fakePasswords{matchesFn: func(hash, password string) bool {
+		return hash == "stored-hash" && password == "right-password"
+	}}
+
+	u, err := svc.Authenticate(context.Background(), " PERSON@EXAMPLE.COM ", "right-password")
+	require.NoError(t, err)
+	assert.Equal(t, stored, u)
+
+	_, err = svc.Authenticate(context.Background(), "person@example.com", "wrong-password")
+	require.ErrorIs(t, err, ErrInvalidCredentials)
+}
+
+func TestAuthenticateHidesMissingUser(t *testing.T) {
+	repo := fakeStore{byEmailFn: func(context.Context, string) (User, error) {
+		return User{}, ErrNotFound
+	}}
+	svc := NewService(repo)
+
+	_, err := svc.Authenticate(context.Background(), "missing@example.com", "password1")
+	require.ErrorIs(t, err, ErrInvalidCredentials)
+}
+
+func TestUserPasswordHashIsNotSerialized(t *testing.T) {
+	u := User{ID: 1, Email: "p@example.com", PasswordHash: "do-not-return", Role: RoleViewer}
+	raw, err := json.Marshal(u)
+	require.NoError(t, err)
+	assert.NotContains(t, string(raw), "do-not-return")
+	assert.NotContains(t, string(raw), "password_hash")
+}

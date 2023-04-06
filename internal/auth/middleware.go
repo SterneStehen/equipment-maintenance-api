015a3d1aad82cec98e4da5d03d517dd41df03add
SterneStehen <122315689+SterneStehen@users.noreply.github.com>
2023-04-06 09:58:52 +0300

    add admin checks in service too

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/SterneStehen/equipment-maintenance-api/internal/apperror"
+	"github.com/SterneStehen/equipment-maintenance-api/internal/user"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,16 +13,18 @@ const principalKey = "auth.principal"
 
 func (m *Manager) Middleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Fields also deals with the weird extra spaces clients sometimes send
+		// Fields handles the extra spaces people somehow manage to send
 		bits := strings.Fields(c.GetHeader("Authorization"))
 		if len(bits) != 2 || !strings.EqualFold(bits[0], "Bearer") {
 			apperror.Write(c, http.StatusUnauthorized, "unauthorized", "Authentication is required")
+			c.Abort()
 			return
 		}
 
 		who, err := m.Verify(bits[1])
 		if err != nil {
 			apperror.Write(c, http.StatusUnauthorized, "unauthorized", "Access token is invalid or expired")
+			c.Abort()
 			return
 		}
 		c.Set(principalKey, who)
@@ -36,4 +39,23 @@ func Current(c *gin.Context) (Principal, bool) {
 	}
 	p, ok := v.(Principal)
 	return p, ok
+}
+
+func RequireRole(roles ...user.Role) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		p, ok := Current(c)
+		if !ok {
+			apperror.Write(c, http.StatusUnauthorized, "unauthorized", "Authentication is required")
+			c.Abort()
+			return
+		}
+		for i := range roles {
+			if p.Role == roles[i] {
+				c.Next()
+				return
+			}
+		}
+		apperror.Write(c, http.StatusForbidden, "forbidden", "You do not have permission to use this endpoint")
+		c.Abort()
+	}
 }

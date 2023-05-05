260ac9b460eb9297665a619ae526a516f0283426
SterneStehen <122315689+SterneStehen@users.noreply.github.com>
2023-05-05 18:49:31 +0300

    fix migration rollback

--- internal/database/database_integration_test.go
+++ internal/database/database_integration_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-func TestPostgreSQLPoolAndUsersMigrationLifecycle(t *testing.T) {
+func TestPostgreSQLPoolAndMigrationLifecycle(t *testing.T) {
 	databaseURL := os.Getenv("TEST_DATABASE_URL")
 	if databaseURL == "" {
 		t.Skip("TEST_DATABASE_URL is not set")
@@ -50,18 +50,20 @@ func TestPostgreSQLPoolAndUsersMigrationLifecycle(t *testing.T) {
 	if err := migrator.Up(); err != nil {
 		t.Fatalf("apply migrations: %v", err)
 	}
-	assertMigrationVersion(t, migrator, 1)
+	assertMigrationVersion(t, migrator, 2)
 	assertUsersConstraints(t, ctx, pool)
+	assertEquipmentConstraints(t, ctx, pool)
 
 	if err := migrator.Down(); err != nil {
 		t.Fatalf("roll back migrations: %v", err)
 	}
 	assertUsersTableMissing(t, ctx, pool)
+	assertEquipmentTableMissing(t, ctx, pool)
 
 	if err := migrator.Up(); err != nil {
 		t.Fatalf("reapply migrations: %v", err)
 	}
-	assertMigrationVersion(t, migrator, 1)
+	assertMigrationVersion(t, migrator, 2)
 }
 
 func openTestMigrator(t *testing.T, databaseURL string) *migrate.Migrate {
@@ -137,6 +139,51 @@ func assertUsersConstraints(t *testing.T, ctx context.Context, pool *pgxpool.Poo
 	}
 }
 
+func assertEquipmentConstraints(t *testing.T, ctx context.Context, pool *pgxpool.Pool) {
+	t.Helper()
+	var cols int
+	if err := pool.QueryRow(ctx, `
+		SELECT COUNT(*)
+		FROM information_schema.columns
+		WHERE table_schema = 'public'
+		  AND table_name = 'equipment'
+		  AND column_name IN ('id', 'serial_number', 'name', 'model', 'location', 'status', 'notes', 'created_at', 'updated_at', 'decommissioned_at')
+	`).Scan(&cols); err != nil {
+		t.Fatalf("inspect equipment columns: %v", err)
+	}
+	if cols != 10 {
+		t.Fatalf("equipment expected column count = %d, want 10", cols)
+	}
+
+	if _, err := pool.Exec(ctx, `
+		INSERT INTO equipment (serial_number, name, status)
+		VALUES ('PUMP-100', 'Pump 100', 'active')
+	`); err != nil {
+		t.Fatalf("insert valid equipment: %v", err)
+	}
+
+	if _, err := pool.Exec(ctx, `
+		INSERT INTO equipment (serial_number, name, status)
+		VALUES ('PUMP-100', 'Duplicate Pump', 'active')
+	`); err == nil {
+		t.Fatal("duplicate serial number was accepted")
+	}
+
+	if _, err := pool.Exec(ctx, `
+		INSERT INTO equipment (serial_number, name, status)
+		VALUES ('not-normalized', 'Bad Serial', 'active')
+	`); err == nil {
+		t.Fatal("non-normalized serial number was accepted")
+	}
+
+	if _, err := pool.Exec(ctx, `
+		INSERT INTO equipment (serial_number, name, status)
+		VALUES ('PUMP-101', 'Bad Status', 'gone')
+	`); err == nil {
+		t.Fatal("invalid equipment status was accepted")
+	}
+}
+
 func assertUsersTableMissing(t *testing.T, ctx context.Context, pool *pgxpool.Pool) {
 	t.Helper()
 	var tableName *string
@@ -145,5 +192,16 @@ func assertUsersTableMissing(t *testing.T, ctx context.Context, pool *pgxpool.Po
 	}
 	if tableName != nil {
 		t.Fatalf("users table still exists after rollback: %s", *tableName)
+	}
+}
+
+func assertEquipmentTableMissing(t *testing.T, ctx context.Context, pool *pgxpool.Pool) {
+	t.Helper()
+	var tableName *string
+	if err := pool.QueryRow(ctx, "SELECT to_regclass('public.equipment')::text").Scan(&tableName); err != nil {
+		t.Fatalf("check equipment table after rollback: %v", err)
+	}
+	if tableName != nil {
+		t.Fatalf("equipment table still exists after rollback: %s", *tableName)
 	}
 }
